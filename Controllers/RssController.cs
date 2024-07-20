f8def39c2e54cba7170fef3faef3cfe4ea4841b6
Erzen Krasniqi <njnana2017@gmail.com>
2024-07-20 02:15:35 +0200

    Added RSS Controller

---
+++ Controllers/RssController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsAggregation.Services;
+
+namespace NewsAggregation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RssController : ControllerBase
+    {
+        private readonly RssService _rssFeedService;
+
+        public RssController(RssService rssFeedService)
+        {
+            _rssFeedService = rssFeedService;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string url)
+        {
+            var items = _rssFeedService.ParseRssFeed(url);
+            return Ok(items);
+        }
+    }
+}
---
+++ DTO/RssArticle.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsAggregation.DTO
+{
+    public class RssArticle
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+        public string Creator { get; set; }
+        public string Image { get; set; }
+        public string Link { get; set; }
+    }
+}

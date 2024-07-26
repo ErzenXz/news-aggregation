8c89009e90f0f9023dcb33dbe7b6b2c32c9c613f
Erzen Krasniqi <48888391+ErzenXz@users.noreply.github.com>
2024-07-26 09:27:50 +0200

    Update ScapeNewsSourcesService.cs

--- Services/ServiceJobs/ScapeNewsSourcesService.cs
+++ Services/ServiceJobs/ScapeNewsSourcesService.cs
@@ -44,102 +44,84 @@ namespace NewsAggregation.Services.ServiceJobs
 
         }
 
-        private async void DoWork(object state)
+       private async void DoWork(object state)
+{
+    _logger.LogInformation("[x] Fetching news sources.");
+    using (var scope = _serviceScopeFactory.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<DBContext>();
+
+        try
         {
-            _logger.LogInformation("[x] Fetching news sources.");
-            using (var scope = _serviceScopeFactory.CreateScope())
+            await _retryPolicy.ExecuteAsync(async () =>
             {
-                var dbContext = scope.ServiceProvider.GetRequiredService<DBContext>();
+                _logger.LogInformation("[x] Fetching news sources for changes.");
 
-                try
+                var sources = await dbContext.Sources.AsNoTracking().ToListAsync();
+                var newArticles = new ConcurrentBag<Article>();
+                var existingTitles = await dbContext.Articles.AsNoTracking()
+                    .Select(a => a.Title)
+                    .ToListAsync();
+
+                var maxDegreeOfParallelism = 10;
+                await Parallel.ForEachAsync(sources, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism }, async (source, token) =>
                 {
-                    await _retryPolicy.ExecuteAsync(async () =>
-                            {
-                                // Get all sources from the database
-                                var sources = await dbContext.Sources.AsNoTracking().ToListAsync();
+                    var items = await RssService.ParseRssFeed(source.Url);
+                    var articlesToAdd = items
+                        .Where(item => !existingTitles.Contains(item.Title))
+                        .Select(item => new Article
+                        {
+                            Title = item.Title ?? "No Title",
+                            Description = item.Description ?? "No Description",
+                            ImageUrl = item.Image ?? "https://via.placeholder.com/500",
+                            Url = item.Link ?? "https://example.com",
+                            SourceId = source.Id,
+                            AuthorId = new Guid("46611d66-cbd8-4a2f-9f9c-527edeab3984"),
+                            CategoryId = 1,
+                            Views = 0,
+                            Likes = 0,
+                            IsPublished = true,
+                            PublishedAt = DateTime.UtcNow,
+                            Tags = string.Join(",", (item.Description ?? string.Empty).Split(' ')
+                                .GroupBy(x => x)
+                                .OrderByDescending(x => x.Count())
+                                .Select(x => x.Key)
+                                .Take(10)
+                                .Where(tag => !string.IsNullOrWhiteSpace(tag))),
+                            CreatedAt = DateTime.UtcNow,
+                            UpdatedAt = DateTime.UtcNow,
+                            Content = item.Content ?? "No Content"
+                        })
+                        .ToList();
 
-                                // Fetch the RSS feeds for all sources in parallel
-                                var fetchTasks = sources.Select(source =>
-                                    RssService.ParseRssFeed(source.Url)
-                                        .ContinueWith(t => new { Source = source, Items = t.Result })
-                                ).ToList();
-
-                                var results = await Task.WhenAll(fetchTasks);
-
-                                var newArticles = new List<Article>();
-
-                                // Combine all titles from the items
-                                var allItems = results.SelectMany(r => r.Items).ToList();
-                                var allTitles = allItems.Select(i => i.Title).ToList();
-
-                                // Batch check if the articles already exist in the database
-                                var existingTitles = await dbContext.Articles.AsNoTracking()
-                                    .Where(a => allTitles.Contains(a.Title))
-                                    .Select(a => a.Title)
-                                    .ToListAsync();
-
-                                // Filter out existing articles and prepare new articles list
-                                foreach (var result in results)
-                                {
-                                    var source = result.Source;
-                                    var items = result.Items;
-
-                                    var articlesToAdd = items
-                                        .Where(item => !existingTitles.Contains(item.Title))
-                                        .Select(item => new Article
-                                        {
-                                            Title = item.Title ?? "No Title",
-                                            Description = item.Description ?? "No Description",
-                                            ImageUrl = item.Image ?? "https://via.placeholder.com/500",
-                                            Url = item.Link ?? "https://example.com",
-                                            SourceId = source.Id,
-                                            AuthorId = new Guid("46611d66-cbd8-4a2f-9f9c-527edeab3984"),
-                                            CategoryId = 1,
-                                            Views = 0,
-                                            Likes = 0,
-                                            IsPublished = true,
-                                            PublishedAt = DateTime.UtcNow,
-                                            Tags = string.Join(",", (item.Description ?? string.Empty).Split(' ')
-                                                .GroupBy(x => x)
-                                                .OrderByDescending(x => x.Count())
-                                                .Select(x => x.Key)
-                                                .Take(10)
-                                                .Where(tag => !string.IsNullOrWhiteSpace(tag))),
-                                            CreatedAt = DateTime.UtcNow,
-                                            UpdatedAt = DateTime.UtcNow,
-                                            Content = item.Content ?? "No Content"
-                                        })
-                                        .ToList();
-
-                                    newArticles.AddRange(articlesToAdd);
-                                }
-
-                                if (newArticles.Any())
-                                {
-                                    await dbContext.Articles.AddRangeAsync(newArticles);
-                                    await dbContext.SaveChangesAsync();
-                                    _logger.LogInformation($"{newArticles.Count} articles added to the database.");
-                                }
-                            });
-
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"An error occurred while processing sources: {ex.Message}");
-                    if (ex.InnerException != null)
+                    foreach (var article in articlesToAdd)
                     {
-                        _logger.LogError($"Inner exception: {ex.InnerException.Message}");
+                        newArticles.Add(article);
                     }
-                }
-                finally
+                });
+
+                if (newArticles.Any())
                 {
-                    if (dbContext != null)
-                    {
-                        await dbContext.DisposeAsync();
-                    }
+                    await dbContext.Articles.AddRangeAsync(newArticles);
+                    await dbContext.SaveChangesAsync();
+                    _logger.LogInformation($"{newArticles.Count} articles added to the database.");
                 }
+            });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"An error occurred while processing sources: {ex.Message}");
+            if (ex.InnerException != null)
+            {
+                _logger.LogError($"Inner exception: {ex.InnerException.Message}");
             }
         }
+        finally
+        {
+            await dbContext.DisposeAsync();
+        }
+    }
+}
 
 
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)

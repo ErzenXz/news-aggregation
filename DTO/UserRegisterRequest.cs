cff116d8d10960a54f4d557394a0dc5995a4f381
Erzen Krasniqi <njnana2017@gmail.com>
2024-07-27 19:43:55 +0200

    Removed unnecessary stuf

--- DTO/UserRegisterRequest.cs
+++ DTO/UserRegisterRequest.cs
@@ -6,9 +6,6 @@
         public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public DateTime FirstLogin { get; set; }
-        public DateTime LastLogin { get; set; }
-        public string? ConnectingIp { get; set; }
         public DateTime? Birthdate { get; set; }
         public string? Language { get; set; }
         public string? TimeZone { get; set; }

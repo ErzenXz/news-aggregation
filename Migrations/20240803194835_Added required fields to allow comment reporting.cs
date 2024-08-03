35b779905e313ed17b156586772e22f49efe0e5e
Erzen Krasniqi <njnana2017@gmail.com>
2024-08-03 22:01:26 +0200

    Added in DB

---
+++ Migrations/20240803194835_Added required fields to allow comment reporting.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsAggregation.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedrequiredfieldstoallowcommentreporting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReported",
+                table: "Comments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReportCount",
+                table: "Comments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReported",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ReportCount",
+                table: "Comments");
+        }
+    }
+}
